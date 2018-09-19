d2405b003f46156fb3c1ac1755a21310a5b1885e
Nagendra Kumar <nagendra.nekkalapu@fadv.com>
2018-09-19 19:55:35 +0530

    test added.

--- pet-clinic-web/src/test/java/org/pet/clinic/web/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/org/pet/clinic/web/SfPetClinicApplicationTests.java
@@ -1,11 +1,13 @@
-package com.vivek.spring5;
+package org.pet.clinic.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration
 @SpringBootTest
 public class SfPetClinicApplicationTests {
 
